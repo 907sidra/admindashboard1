0ca5fc9f4e6bdd26119231d7448bdecf4e6b9cca
907sidra <haqsidra907@gmail.com>
2025-02-07 17:13:41 +0500

    final changes

--- 14.2.2/sanity/env.ts
+++ 14.2.2/sanity/env.ts
@@ -10,6 +10,10 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
+export const token = assertValue(
+  "skjDvSGzPsyHw32Tqle6697aKYY8umBhT2OP9pWiZAqLMdMySxyTEwEtMC36oLOqUS8xswpgmDGLzHPUYelcdVnooIi5bnxpG8gfZxjhAXNLhTsEmJMuTRH95hmV3YF2z6drgKEgj53niuYwbC3sQRvmNXyusnxLqj4pp8K2stKKVmLJdxvE",
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
